7cd5210b4c7bcd2107fb28705fa0732c38b33bb9
oscar <oscarrodrigomez22@gmail.com>
2024-04-25 09:53:28 -0500

    inicial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,11 +39,13 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-auth")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
