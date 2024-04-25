9b26453040ef164171a43de0cc2067bf8389963f
estudiante <plays867yer@gmail.com>
2024-04-25 10:18:25 -0500

    LoadUser

--- app/src/main/java/com/example/taller3_icm/RegistrarUsuarioActivity.kt
+++ app/src/main/java/com/example/taller3_icm/RegistrarUsuarioActivity.kt
@@ -1,17 +1,24 @@
 package com.example.taller3_icm
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class RegistrarUsuarioActivity : AppCompatActivity() {
 
     val PATH_USERS="users/"
     private val database = FirebaseDatabase.getInstance()
     private val auth = FirebaseAuth.getInstance()
+    private lateinit var  myRef:DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_usuario)
@@ -29,5 +36,23 @@ class RegistrarUsuarioActivity : AppCompatActivity() {
             myRef.setValue(usr1)
 
         }
+    }
+
+    fun loadUsers() {
+        myRef = database.getReference(PATH_USERS)
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (singleSnapshot in dataSnapshot.children) {
+                    val myUser = singleSnapshot.getValue(MyUser::class.java)
+                    Log.i("Firebase", "Encontró usuario: " + myUser?.name)
+                    val name = myUser?.name
+                    val age = myUser?.age
+                    Toast.makeText(baseContext, "$name: $age", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("Firebase", "error en la consulta", databaseError.toException())
+            }
+        })
     }
 }
