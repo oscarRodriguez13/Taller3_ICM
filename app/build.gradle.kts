2692855be12d761d86124c094af95b0562f10419
oscar <oscarrodrigomez22@gmail.com>
2024-04-25 09:45:03 -0500

    inicial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,7 +37,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-auth")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/taller3_icm/MainActivity.kt
+++ app/src/main/java/com/example/taller3_icm/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.taller3_icm
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        auth = Firebase.auth
+
+        val editTextTextEmailAddress = findViewById<EditText>(R.id.email_input)
+        val editTextTextPassword = findViewById<EditText>(R.id.password_input)
+
+        val email = editTextTextEmailAddress.text.toString()
+        val password = editTextTextPassword.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Email y contraseña correctos", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Email y contraseña incorrectos", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } else {
+            // Mostrar mensaje de error al usuario
+            Toast.makeText(this, "Email y contraseña son requeridos", Toast.LENGTH_SHORT).show()
         }
+
     }
 }
